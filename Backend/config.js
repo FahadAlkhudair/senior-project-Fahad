10a236c64c5a74bf430e4933bb91a45c1baa7c4c
FahadAlkhudair <falkhudair@ad.stetosn.edu>
2022-06-13 21:07:03 -0400

    Create config.js

---
+++ Backend/config.js
@@ -0,0 +1,7 @@
+module.exports={
+    cors:{
+        origin: "http://localhost:8081"
+    },
+    mongoDBConectionString: "mongodb+srv://falkhudair:Leopard-321@cluster0.golvl.mongodb.net/?retryWrites=true&w=majority",
+    
+}
