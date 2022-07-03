c35aa3dffb7586578e07b36fc1a4f123ec14a2f6
FahadAlkhudair <falkhudair@ad.stetosn.edu>
2022-07-02 23:05:17 -0400

    Update index.js

--- Backend/models/index.js
+++ Backend/models/index.js
@@ -1,13 +1,27 @@
 const mongoose = require('mongoose');
 const db = {};
 
-// User Management - Login and Profiles
+// User Management
 db.user = require('./auth/user.model');
 db.profile = require('./auth/profile.model');
 db.refreshToken = require('./auth/refreshToken.model');
 
+// Blood Component Management 
+db.donations = require('./blood_component/donation.model');
+
+// Donor Managment
+db.bloodDonation = require('./donor_management/bloodDonation.model');
+db.questionnaire = require('./donor_management/questionnaire.model');
+db.examResult = require('./donor_management/examResult.model');
+db.bloodDrive = require('./donor_management/bloodDrive.model');
+db.bloodDriveSlot = require('./donor_management/bloodDriveSlot.model');
+db.appointment = require('./donor_management/appointment.model');
+
 // Enums
 db.ROLES = require('./auth/roles.model');
+db.BLOOD_TYPES = require('./blood_component/bloodTypes.model');
+db.DONATION_TYPES = require('./blood_component/donationTypes.model');
+db.RESULT_STATUSES = require('./donor_management/resultStatuses.model');
 
 db.mongoose = mongoose;
 
